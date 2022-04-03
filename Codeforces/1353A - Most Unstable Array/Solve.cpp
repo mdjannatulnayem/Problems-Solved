ca0aeb800fd1cb5e7afd685a74b833b5b83b607e
Md. Jannatul Nayem <64035356+mdjannatulnayem@users.noreply.github.com>
2022-04-03 16:51:45 +0600

    Create Solve.cpp

---
+++ Codeforces/1353A - Most Unstable Array/Solve.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int T,n,m;
+    cin >> T;
+    while (T>0) {
+        cin >> n >> m;
+        //Pending....!
+        T--;
+    }
+    return 0;
+}
