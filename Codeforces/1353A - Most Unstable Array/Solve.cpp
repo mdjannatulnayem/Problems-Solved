79fc57ec5a679eff977177d2f435a178cb4629c4
Md. Jannatul Nayem <64035356+mdjannatulnayem@users.noreply.github.com>
2022-04-04 09:56:17 +0600

    Update Solve.cpp

--- Codeforces/1353A - Most Unstable Array/Solve.cpp
+++ Codeforces/1353A - Most Unstable Array/Solve.cpp
@@ -3,13 +3,29 @@
 #include <algorithm>
 using namespace std;
 
-int main(){
-    int T,n,m;
-    cin >> T;
-    while (T>0) {
-        cin >> n >> m;
-        //Pending....!
-        T--;
-    }
-    return 0;
+int main() {
+	int T,n,a,m;
+	cin >> T;
+	while(T--)
+	{
+	    cin >> n >> m;
+	    if(n==1)
+	    {
+	        cout << "0" << endl;
+	    }
+	    else if(n==2)
+	    {
+	        cout << m << endl;
+	    }
+	    else if(n==3)
+	    {
+	        cout << 2*m << endl;
+	    }
+	    else
+	    {
+	        a = m/2;
+	        cout << ((2*a)+2*abs(m-a)) << endl;
+	    }
+	}
+	return 0;
 }
