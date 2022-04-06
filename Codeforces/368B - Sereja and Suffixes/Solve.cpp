f71b1bd6641270ff037894cebdfadfc2fa7eabb4
Md. Jannatul Nayem <64035356+mdjannatulnayem@users.noreply.github.com>
2022-04-06 17:50:08 +0600

    Update Solve.cpp

--- Codeforces/368B - Sereja and Suffixes/Solve.cpp
+++ Codeforces/368B - Sereja and Suffixes/Solve.cpp
@@ -1,1 +1,26 @@
+//GETS TLE!
 
+#include <set>
+#include <iostream>
+using namespace std;
+
+int main() {
+	set<int> s;
+	int n,m,k;
+	int *p=NULL;
+	cin >> n >> m;
+	p=new int[n];
+	for(int i=0;i<n;i++){
+		cin >> p[i];
+	}
+	while(m--){
+		cin >> k;
+		for(int i=k-1;i<n;i++){
+			s.insert(p[i]);
+		}
+		cout << s.size() << endl;
+		s.clear();
+	}
+	delete [] p;
+	return 0;
+}
